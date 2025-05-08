da564e3e613a7061712032d0352f110a5e57faaf
aristides1000 <aristides1000@gmail.com>
2025-05-08 19:37:05 +0000

    Feat: Add Saldo zelle to modulo_saldo.py

--- modulo_saldo.py
+++ modulo_saldo.py
@@ -73,6 +73,21 @@ def calcular_saldo_efectivo():
   finally:
     conn.close()
 
+def calcular_saldo_zelle():
+  try:
+    conn = sqlite3.connect("floristeria.db")
+    cursor = conn.cursor()
+    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
+    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
+    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'Zelle'", (fecha_hora_inicial, fecha_hora_final))
+    resultado = cursor.fetchone()[0]
+    saldo_zelle = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
+    saldo_zelle_var.set(round(saldo_zelle, 2))  # Actualizar la variable con el costo acumulado
+  except sqlite3.Error as e:
+    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
+  finally:
+    conn.close()
+
 def calcular_costo_acumulado_rango_fechas():
   try:
     conn = sqlite3.connect("floristeria.db")
@@ -88,6 +103,7 @@ def calcular_costo_acumulado_rango_fechas():
     calcular_saldo_transferencia()
     calcular_saldo_pago_movil()
     calcular_saldo_efectivo()
+    calcular_saldo_zelle()
 
   except sqlite3.Error as e:
     messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
@@ -104,6 +120,7 @@ def limpiar_campos():
   saldo_transferencia_var.set(0.0)
   saldo_pago_movil_var.set(0.0)
   saldo_efectivo_var.set(0.0)
+  saldo_zelle_var.set(0.0)
 
 # Configuración de la ventana principal
 root = tk.Tk()
@@ -149,7 +166,7 @@ ttk.Button(btn_frame, text="Limpiar Campos", command=limpiar_campos).pack(side="
 labels = [
     "Fecha Inicial", "Hora Inicial (HH:MM)", "Fecha Final",
     "Hora Final (HH:MM)", "Saldo Transferencia", "Saldo Pago movil",
-    "Saldo Efectivo", "Costo Total"
+    "Saldo Efectivo", "Saldo Zelle", "Costo Total"
 ]
 
 row_index = 0
@@ -192,6 +209,12 @@ for i, text in enumerate(labels):
     saldo_efectivo_var = tk.DoubleVar(value=0.0)
     ttk.Label(form_frame, text="Saldo Efectivo:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
     ttk.Label(form_frame, textvariable=saldo_efectivo_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
+    row_index += 1
+  elif text == "Saldo Zelle":
+    # Campo para el costo acumulado
+    saldo_zelle_var = tk.DoubleVar(value=0.0)
+    ttk.Label(form_frame, text="Saldo Zelle:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
+    ttk.Label(form_frame, textvariable=saldo_zelle_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
     row_index += 1
   elif text == "Costo Total":
     # Campo para el costo acumulado
