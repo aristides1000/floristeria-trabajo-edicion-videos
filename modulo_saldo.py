95f4296d5408558f5b114a73aa61efcbdb5035d6
aristides1000 <aristides1000@gmail.com>
2025-05-22 16:36:01 +0000

    Feat: develop calcular_saldo_dolares_rango_fechas

--- modulo_saldo.py
+++ modulo_saldo.py
@@ -37,114 +37,18 @@ def conectar_db():
   conn.commit()
   conn.close()
 
-def calcular_saldo_transferencia():
+def calcular_saldo_dolares_rango_fechas():
   try:
     conn = sqlite3.connect("floristeria.db")
     cursor = conn.cursor()
     fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
     fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'Transferencia'", (fecha_hora_inicial, fecha_hora_final))
+    cursor.execute("SELECT SUM(costo_total_dolares) FROM pedidos WHERE fecha_hora BETWEEN ? AND ?", (fecha_hora_inicial, fecha_hora_final))
     resultado = cursor.fetchone()[0]
-    saldo_transferencia = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_transferencia_var.set(round(saldo_transferencia, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_saldo_pago_movil():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'PagoMóvil'", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    saldo_pago_movil = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_pago_movil_var.set(round(saldo_pago_movil, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_saldo_efectivo():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'Efectivo'", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    saldo_efectivo = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_efectivo_var.set(round(saldo_efectivo, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_saldo_zelle():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'Zelle'", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    saldo_zelle = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_zelle_var.set(round(saldo_zelle, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_saldo_por_cobrar():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago = 'Por Cobrar'", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    saldo_por_cobrar = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_por_cobrar_var.set(round(saldo_por_cobrar, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_saldo_total_menos_por_cobrar():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE (fecha_hora BETWEEN ? AND ?) AND forma_pago <> 'Por Cobrar'", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    saldo_total_menos_por_cobrar = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    saldo_total_menos_por_cobrar_var.set(round(saldo_total_menos_por_cobrar, 2))  # Actualizar la variable con el costo acumulado
-  except sqlite3.Error as e:
-    messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
-  finally:
-    conn.close()
-
-def calcular_costo_acumulado_rango_fechas():
-  try:
-    conn = sqlite3.connect("floristeria.db")
-    cursor = conn.cursor()
-    fecha_hora_inicial = f"{entry_fecha_inicial.get()} {hora_inicial_var.get()}"
-    fecha_hora_final = f"{entry_fecha_final.get()} {hora_inicial_var.get()}"
-    cursor.execute("SELECT SUM(costo_total) FROM pedidos WHERE fecha_hora BETWEEN ? AND ?", (fecha_hora_inicial, fecha_hora_final))
-    resultado = cursor.fetchone()[0]
-    costo_acumulado = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
-    costo_acumulado_var.set(round(costo_acumulado, 2))  # Actualizar la variable con el costo acumulado
+    costo_acumulado_dolares = resultado if resultado else 0.0  # Si no hay pedidos, el costo es 0.0
+    costo_acumulado_dolares_var.set(round(costo_acumulado_dolares, 2))  # Actualizar la variable con el costo acumulado
 
     # Calculo de otros saldos
-    calcular_saldo_transferencia()
-    calcular_saldo_pago_movil()
-    calcular_saldo_efectivo()
-    calcular_saldo_zelle()
-    calcular_saldo_por_cobrar()
-    calcular_saldo_total_menos_por_cobrar()
 
   except sqlite3.Error as e:
     messagebox.showerror("Error", f"Ocurrió un error al calcular el costo acumulado: {fecha_hora_inicial} {fecha_hora_final} {e}")
@@ -157,13 +61,7 @@ def limpiar_campos():
   hora_inicial_var.set("08:00")
   entry_fecha_final.set_date(datetime.now().date())
   hora_final_var.set("08:00")
-  costo_acumulado_var.set(0.0)
-  saldo_transferencia_var.set(0.0)
-  saldo_pago_movil_var.set(0.0)
-  saldo_efectivo_var.set(0.0)
-  saldo_zelle_var.set(0.0)
-  saldo_por_cobrar_var.set(0.0)
-  saldo_total_menos_por_cobrar_var.set(0.0)
+  costo_acumulado_dolares_var.set(0.0)
 
 # Configuración de la ventana principal
 root = tk.Tk()
@@ -202,15 +100,13 @@ hora_final_var = tk.StringVar()
 # Botones
 btn_frame = ttk.Frame(main_frame)
 btn_frame.pack(fill="x", pady=10)
-ttk.Button(btn_frame, text="Ver Costo Total", command=calcular_costo_acumulado_rango_fechas).pack(side="left", padx=5)
+ttk.Button(btn_frame, text="Ver Costo Total", command=calcular_saldo_dolares_rango_fechas).pack(side="left", padx=5)
 ttk.Button(btn_frame, text="Limpiar Campos", command=limpiar_campos).pack(side="left", padx=5)
 
 # Bucle para crear los campos del formulario
 labels = [
     "Fecha Inicial", "Hora Inicial (HH:MM)", "Fecha Final",
-    "Hora Final (HH:MM)", "Saldo Transferencia", "Saldo Pago Movil",
-    "Saldo Efectivo", "Saldo Zelle", "Saldo Por Cobrar",
-    "Saldo Total Menos Por Cobrar", "Costo Total"
+    "Hora Final (HH:MM)", "Costo Total"
 ]
 
 row_index = 0
@@ -236,47 +132,11 @@ for i, text in enumerate(labels):
     hora_final_var.grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
     hora_final_var.current(0)
     row_index += 1
-  elif text == "Saldo Transferencia":
-    # Campo para el costo acumulado
-    saldo_transferencia_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Transferencia:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_transferencia_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
-  elif text == "Saldo Pago Movil":
-    # Campo para el costo acumulado
-    saldo_pago_movil_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Pago Movil:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_pago_movil_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
-  elif text == "Saldo Efectivo":
-    # Campo para el costo acumulado
-    saldo_efectivo_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Efectivo:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_efectivo_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
-  elif text == "Saldo Zelle":
-    # Campo para el costo acumulado
-    saldo_zelle_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Zelle:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_zelle_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
-  elif text == "Saldo Por Cobrar":
-    # Campo para el costo acumulado
-    saldo_por_cobrar_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Por Cobrar:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_por_cobrar_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
-  elif text == "Saldo Total Menos Por Cobrar":
-    # Campo para el costo acumulado
-    saldo_total_menos_por_cobrar_var = tk.DoubleVar(value=0.0)
-    ttk.Label(form_frame, text="Saldo Total Menos Por Cobrar:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=saldo_total_menos_por_cobrar_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
-    row_index += 1
   elif text == "Costo Total":
     # Campo para el costo acumulado
-    costo_acumulado_var = tk.DoubleVar(value=0.0)
+    costo_acumulado_dolares_var = tk.DoubleVar(value=0.0)
     ttk.Label(form_frame, text="Costo Total:", anchor="w").grid(row=row_index, column=0, padx=5, pady=5, sticky="w")
-    ttk.Label(form_frame, textvariable=costo_acumulado_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
+    ttk.Label(form_frame, textvariable=costo_acumulado_dolares_var, anchor="w").grid(row=row_index, column=1, padx=5, pady=5, sticky="w")
     row_index += 1
 
 # Ejecutar la aplicación
